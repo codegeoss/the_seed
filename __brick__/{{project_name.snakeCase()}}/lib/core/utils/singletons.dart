902b641448bc8bbb87771c52b8130aea4b16ef07
codegeass <denzelgatugu@outlook.com>
2026-03-27 20:45:49 +0300

    Move `CounterCubit` registration to `Singletons` and refactor `CounterPage`

--- __brick__/{{project_name.snakeCase()}}/lib/core/utils/singletons.dart
+++ __brick__/{{project_name.snakeCase()}}/lib/core/utils/singletons.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Singletons {
-  static List<BlocProvider> registerCubits() => [];
+  static List<BlocProvider> registerCubits() => [
+    BlocProvider(create: (context) => CounterCubit()),
+  ];
 }
